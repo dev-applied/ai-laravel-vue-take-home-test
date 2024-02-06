558fd8c4d0924837f942222fea5122efdbe467e5
Devin345458 <devin@appliedimagination.com>
2024-02-06 17:44:25 -0600

    Minor fixes for useAutoComplete

--- resources/composables/useAutoComplete.ts
+++ resources/composables/useAutoComplete.ts
@@ -1,17 +1,28 @@
 import { ref, watch } from 'vue'
 const apiKey = import.meta.env.VITE_GEOAPIFY_API_KEY
+import { useDebounceFn } from '@vueuse/core'
 
 export default () => {
   const addressSearch = ref('')
-  const searchResults = ref([])
-  const searchError = ref(null)
+  const addressResults = ref([])
+  const addressError = ref(null)
 
-  watch(addressSearch, async (search: string) => {
-    if (!search) return
-    search = encodeURIComponent(search)
-    await fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${search}&apiKey=${apiKey}`)
+  const search = useDebounceFn(() => {
+    if (!addressSearch.value) return
+    const search = encodeURIComponent(addressSearch.value)
+    fetch(
+      `https://api.geoapify.com/v1/geocode/autocomplete?text=${search}&apiKey=${apiKey}&format=json&filter=countrycode:us`
+    )
       .then((res) => res.json())
-      .then((json) => (searchResults.value = json))
-      .catch((err) => (searchError.value = err))
-  })
+      .then((json) => (addressResults.value = json.results))
+      .catch((err) => (addressError.value = err))
+  }, 1000)
+
+  watch(addressSearch, search)
+
+  return {
+    addressSearch,
+    addressResults,
+    addressError
+  }
 }
